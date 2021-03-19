37c7cc9aef3aad363464d32e05fc2df6f00061b7
Jongyoon Jeong <wormwlrm@naver.com>
2021-03-20 01:05:20 +0900

    백준 2798번

--- BOJ/2798 블랙잭/answer.py
+++ BOJ/2798 블랙잭/answer.py
@@ -1,12 +1,14 @@
 n, m = map(int, input().split())
 
+cards = list(map(int, input().split()))
+
 maximum = 0
 
-for i in range(0, n - 1):
-    for j in range(i, n):
-        for k in range(j, n + 1):
-            current = i + j + k
-            if (current > maximum):
+for i in range(0, n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            current = cards[i] + cards[j] + cards[k]
+            if (current > maximum) and (current <= m):
                 maximum = current
 
 print(maximum)
