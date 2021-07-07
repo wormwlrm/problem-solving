9216859d64df7d6352d6aaf981cc479daa009ac6
Jongyoon Jeong <wormwlrm@naver.com>
2021-07-07 11:52:23 +0900

    스크립트 파일 수정

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 fs = require("fs-extra");
 inquirer = require("inquirer");
 
+const [_node, _file, argNo, ...argName] = process.argv;
+
 async function create() {
   const { no } = await inquirer.prompt({
     type: "input",
     name: "no",
     message: "푼 문제 번호",
+    default: argNo,
   });
 
   const { name } = await inquirer.prompt({
     type: "input",
     name: "name",
     message: "푼 문제 이름",
+    default: argName.join(),
   });
 
   const path = `./BOJ/${no} ${name}`;
