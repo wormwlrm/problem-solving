82d7fb12242a3c731b4a73c510eafc350eef386b
Jongyoon Jeong <wormwlrm@naver.com>
2021-08-02 18:16:39 +0900

    백준 2562번

---
+++ BOJ/2562 최댓값/README.md
@@ -0,0 +1,9 @@
+## 2562 최댓값
+
+<https://www.acmicpc.net/problem/2562>
+
+## 내가 생각한 방법
+
+<!-- ![이미지](./img.png) -->
+
+-
---
+++ BOJ/2562 최댓값/answer.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+numbers = []
+
+for _ in range(9):
+    numbers.append(int(input()))
+
+print(max(numbers))
+print(numbers.index(max(numbers)) + 1)
