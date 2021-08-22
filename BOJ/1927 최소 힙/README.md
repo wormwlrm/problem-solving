0d6b5ec1840df36a2b569256b429b3824daac4fb
Jongyoon Jeong <wormwlrm@naver.com>
2021-08-22 16:29:55 +0900

    백준 1927번

---
+++ BOJ/1927 최소 힙/README.md
@@ -0,0 +1,9 @@
+## 1927 최소 힙
+
+<https://www.acmicpc.net/problem/1927>
+
+## 내가 생각한 방법
+
+<!-- ![이미지](./img.png) -->
+
+-
---
+++ BOJ/1927 최소 힙/answer.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = []
+
+
+def zero():
+    if len(arr) == 0:
+        print(0)
+        return
+
+    print(heapq.heappop(arr))
+
+
+for _ in range(N):
+    command = int(input())
+
+    if command == 0:
+        zero()
+    else:
+        heapq.heappush(arr, command)
