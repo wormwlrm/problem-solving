cc82c82bb7d212e9cb554d7cf7b08b99f8614123
Jongyoon Jeong <wormwlrm@naver.com>
2021-08-30 17:15:25 +0900

    HTTP 주소만으로 문제 입력받을 수 있는 커맨드 추가

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 fs = require("fs-extra");
 inquirer = require("inquirer");
+fetch = require("node-fetch");
 
-const [_node, _file, argNo, ...argName] = process.argv;
+const [_node, _file, argNoOrAddress, ...argName] = process.argv;
 
-async function create() {
+async function getManual() {
   const { no } = await inquirer.prompt({
     type: "input",
     name: "no",
     message: "푼 문제 번호",
-    default: argNo,
+    default: argNoOrAddress,
   });
 
   const { name } = await inquirer.prompt({
@@ -18,6 +19,30 @@ async function create() {
     default: argName.join(" "),
   });
 
+  return { no, name };
+}
+
+async function getAuto() {
+  const response = await fetch(argNoOrAddress, {
+    method: "get",
+  }).then((r) => {
+    return r.text();
+  });
+
+  const [noAndName] = /(?<=<title>).*(?=<\/title>)/gi.exec(response);
+  const [no, name] = noAndName.split("번: ");
+
+  return {
+    no,
+    name,
+  };
+}
+
+function isHttpAddress(string) {
+  return /https\:/.test(string);
+}
+
+function copyTemplate(no, name) {
   const path = `./BOJ/${no} ${name}`;
   fs.copySync("./template", path);
 
@@ -30,6 +55,14 @@ async function create() {
       if (err) return console.log(err);
     });
   });
+}
+
+async function create() {
+  const { no, name } = isHttpAddress(argNoOrAddress)
+    ? await getAuto()
+    : await getManual();
+
+  copyTemplate(no, name);
 }
 
 create();
