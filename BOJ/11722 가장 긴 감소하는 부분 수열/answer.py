ccacc4094981a58498c477a737e88630a2a8306f
Jongyoon Jeong <wormwlrm@naver.com>
2021-09-08 19:32:13 +0900

    백준 11722번

---
+++ BOJ/11722 가장 긴 감소하는 부분 수열/answer.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+numbers = [float("inf")] + list(map(int, input().split()))
+
+dp = [0 for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    current = numbers[i]
+
+    maximum = -1
+
+    # 자기보다 앞에 있는 수
+    for j in range(i):
+        if numbers[j] > current:
+            maximum = max(maximum, dp[j])
+
+    dp[i] = maximum + 1
+
+print(max(dp))
