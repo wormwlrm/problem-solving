e4b0def747cd64d39cbfbf1df2b7d1ddd1239854
Jongyoon Jeong <wormwlrm@naver.com>
2021-10-10 22:06:14 +0900

    백준 13398번

--- BOJ/13398 연속합 2/answer.py
+++ BOJ/13398 연속합 2/answer.py
@@ -1,3 +1,5 @@
+# https://github.com/wormwlrm/problem-solving/tree/master/BOJ/13398%20%EC%97%B0%EC%86%8D%ED%95%A9%202
+
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
@@ -6,21 +8,26 @@ N = int(input())
 
 numbers = list(map(int, input().split()))
 
-
-# zero padding, 나중에 잘라냄
+# dp1은 연속된 숫자를 고르는 경우, N번째까지 고른 경우에서 가장 큰 값
 dp_1 = [numbers[0]]
 
 for i in range(1, N):
+    # dp_1[i - 1] + numbers[i]: 이전까지의 누적값 + 현재값
+    # numbers[i]: 단순 현재값, 위 값이 음수일 수 있기 때문
     maximum = max(dp_1[i - 1] + numbers[i], numbers[i])
     dp_1.append(maximum)
 
-# zero padding, 나중에 잘라냄
-dp_2 = [dp_1[0]]
+
+## dp2는 숫자 1개를 뺀 경우
+dp_2 = []
 
 # 3개부터 의미 있음
 if len(numbers) > 2:
+    dp_2.append(dp_1[0])
+
     for i in range(2, len(numbers)):
-        # i - 1 번째를 뺌
+        # dp_1[i - 2] + numbers[i]: i-2번째까지의 최대값 + 현재값, 즉 i-1번째를 뺀 거
+        # dp_2[-1] + numbers[i]: 이전에 i-1번째가 제거된 값 + 현재값, 즉 i-N번째가 빠진 값
         maximum = max(dp_1[i - 2] + numbers[i], dp_2[-1] + numbers[i])
         dp_2.append(maximum)
 
--- template/answer.py
+++ template/answer.py
@@ -1,3 +1,5 @@
+# https://github.com/wormwlrm/problem-solving/tree/master/BOJ/{{ no }}%20{{ name }}
+
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
